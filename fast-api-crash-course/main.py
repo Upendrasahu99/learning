503b85dfec5edb3a8e4b0fb4c3fff316a80d0daf
Upendra Sahu <upendrasahu1199@gmail.com>
2026-03-16 23:57:17 +0530

    update

--- fast-api-crash-course/main.py
+++ fast-api-crash-course/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+items = [
+  {'id': 1, 'name':'item 1'},
+  {'id': 2, 'name':'item 2'},
+]
+
 @app.get("/")
 def health_check():
   return{"status": "ok"}
   
+
+@app.get('/items')
+def get_items():
+  return{"items": items}
