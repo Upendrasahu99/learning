6765999c259a7f75519118cd453a2b3134acd804
Upendra Sahu <upendrasahu1199@gmail.com>
2026-03-19 23:52:56 +0530

    basic route practice in fastapi

--- fast-api-crash-course/main.py
+++ fast-api-crash-course/main.py
@@ -1,35 +1,7 @@
 from fastapi import FastAPI
+from app.routes.issues import router as issues_router
+
 app = FastAPI()
 
-items = [
-  {'id': 1, 'name':'item 1'},
-  {'id': 2, 'name':'item 2'},
-]
-
-@app.get("/")
-def health_check():
-  return{"status": "ok"}
-  
-
-@app.get('/items')
-def get_items():
-  return{"items": items}
-
-
-@app.get('/items/{item_id}')
-def get_item(item_id: int):
-  for item in items:
-    if item['id'] == item_id:
-      return item
-  return{'error': "Item not found"}
-
-
-@app.get('/items/')
-def get_items(skip: int = 0, limit: int = 1):
-  return{"skip": skip, "limit": limit}
-
-@app.post('/items')
-def add_item(item: dict):
-  items.append(item)
-  return items
+app.include_router(issues_router)
 
